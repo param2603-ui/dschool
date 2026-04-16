0afc9df03c928ee92b7a86c5511e1e9cdf540914
param <param2603-u>
2026-04-16 19:01:56 +0530

    fix typescript error

--- techdriving/app/components/ScrollProgress.tsx
+++ techdriving/app/components/ScrollProgress.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const ScrollProgress = ({ scrollProgress }) => {
+type Props = {
+  scrollProgress: number;
+};
+
+const ScrollProgress = ({ scrollProgress }: Props) => {
   const progressBarWidth = `${scrollProgress * 100}%`;
 
   return (
-    <div
-      className="fixed top-0 left-0 h-1 bg-gradient-to-r from-yellow-400 to-yellow-600 transition-all duration-300"
-      style={{ width: progressBarWidth }}
-    />
+    <div style={{ width: progressBarWidth }} />
   );
 };
 
